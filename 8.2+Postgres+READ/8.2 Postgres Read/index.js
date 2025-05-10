c81c38a253e1c5bd28d15f3ab97e10b605bdfb71
rafgger <rafgger@gmail.com>
2025-05-10 18:48:11 +0200

    similar to previous exercise | needs as well to initialize quiz

--- 8.2+Postgres+READ/8.2 Postgres Read/index.js
+++ 8.2+Postgres+READ/8.2 Postgres Read/index.js
@@ -1,8 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "password",
+  port: 5432,
+});
+
+db.connect();
+
+let  quiz = [
+  {name: "", flag: ""}
+];
+
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.log("Error executing query", err.stack);    
+  } else {
+    quiz = res.rows;
+  }
+
+  db.end();
+});
 
 let totalCorrect = 0;
 
